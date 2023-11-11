350d1bbfabd0a2c96109e9ad7595bc35356e4fae
Pavla Taterová <taterova.pavla@gmail.com>
2023-11-11 08:54:12 +0100

    Adding basic content¨

--- src/pages/HomePage/components/o-paji/index.jsx
+++ src/pages/HomePage/components/o-paji/index.jsx
@@ -1,8 +1,9 @@
 import { ONas } from '../o-nas';
+import paja from './paja.jpg'
 
-const name = 'Pavla';
-const description = 'Tady něco o tobě';
-const link = 'Odkaz na tvůj github';
+const name = 'Pavla Taterová';
+const description = 'Aktuálně se účastním Digitální akademie Czechitas a jelikož mám kolem sebe v současnosti hodně párů, které už za sebou mají nebo plánují svatbu, říkala jsem si, že by bylo skvělé vytvořit digitálního průvodce svatbou. S mojí teammate Týnkou jsme se rozhodly převést tuto myšlenku na náš společný absolventský projekt.';
+const link = 'https://github.com/taterovapavla';
 // TODO: přidej si obrázek do složky o-paji a obdobně jako v komponentě OKristyne image importuj a následně předej image do komponenty OPaji
 
 export const OPaji = () => {
@@ -11,7 +12,7 @@ export const OPaji = () => {
       name={name}
       description={description}
       link={link}
-      image={null}
+      image={paja}
       key={name}
     />
   );
--- src/pages/HomePage/components/o-projektu/index.jsx
+++ src/pages/HomePage/components/o-projektu/index.jsx
@@ -1,3 +1,8 @@
 export const OProjektu = () => {
-  return <div>O projektu</div>;
+  return (
+    <>
+    <h1>O projektu Svatební itinerář</h1>
+    <p>Cílem projektu je vytvořit web s časovou osou. Uživatel zadá datum svatby a podle tohoto údaje se body na časové ose přerendrují např. na konkrétní měsíce. Když uživatel klikne na konkrétní časový bod (např.leden), zobrazí se mu okno se seznamem věcí (koupit prstýnky, objednat svatební výzdobu), které je třeba zařídit v tomto časovém období. Když uživatel rozklikne konkrétní úkol, dozví se potřebné informace, aby úkol mohl splnit. Přínosem projektu by mělo být usnadnění plánování svatby. Náš web uživatele provede plánováním krok po kroku se všemi důležitými informacemi na jednom místě. </p>
+    </>
+  )
 };
