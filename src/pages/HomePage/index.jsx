9c5c5772701178916966771d4b7166c3bd49e76d
Pavla Taterová <taterova.pavla@gmail.com>
2023-11-12 14:57:20 +0100

    styling navbar

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,23 +8,26 @@ export const HomePage = () => {
   const [selectedComponent, setSelectedComponent] = useState(OProjektu);
   return (
     <>
-      <ul>
-        <li>
-          <button onClick={() => setSelectedComponent(OProjektu)}>
-            O projektu
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setSelectedComponent(OPaji)}>
-            Pavla Taterová
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setSelectedComponent(OKristyne)}>
-            Kristýna Kopalová
-          </button>
-        </li>
-      </ul>
+      <div className='navbar'>
+        <h1 className='navbar__heading'>Svatební itinerář</h1> 
+        <div className='navbar__buttons'>
+          <div className='navbar__button'>
+            <button onClick={() => setSelectedComponent(OProjektu)}>
+              O projektu
+            </button>
+          </div>
+          <div className='navbar__button'>
+            <button onClick={() => setSelectedComponent(OPaji)}>
+              Pavla Taterová
+            </button>
+          </div>
+          <div className='navbar__button'>
+            <button onClick={() => setSelectedComponent(OKristyne)}>
+              Kristýna Kopalová
+            </button>
+          </div>
+        </div>
+      </div>
       {selectedComponent}
     </>
   );
